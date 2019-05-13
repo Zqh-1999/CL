e6ceef6efeae060a54d307cee644e286170a9562
XudanHuazai <2427873127@qq.com>
2019-05-13 14:42:17 +0800

    XuDan

--- XuDan/app.js
+++ XuDan/app.js
@@ -1,46 +1,31 @@
-//引入模块包
-const express = require('express')
-//创建服务器
+const express = require('express');
+
+// 创建服务器
 const app = express();
-//网站图标
-// const favicon = require('serve-favicon');
-// app.use(favicon(__dirname + '/favicon.ico'));
 
+// 服务器配置
+// 0.配置post请求的body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//服务器配置
-const bodyparser = require('body-parser')
-app.use(bodyparser.urlencoded({extended:false}))
-
-//挂载路由
-//添加新用户
-const AddNewAddress = require('./route/routeAddNewAddress')
-app.use(AddNewAddress)
-
-// 我的账户
-const MyAccount = require('./route/routeMyAccount')
-app.use(MyAccount)
-
-// 我的订单
-const MyOrders  = require('./route/routeMyOrders')
-app.use(MyOrders)
-
-// 我的收藏
-const MyWishlist = require('./route/routeMyWishlist')
-app.use(MyWishlist)
-
-//  通讯订阅
-const NewsletterSubscription = require('./route/routeNewsletterSubscription')
-app.use(NewsletterSubscription)
-
-
-//设置静态资源目录
+// 1.设置静态资源目录
 app.use(express.static('./public'));
 
-app.set('view engine', 'ejs');
-// 3.3 设置模板引擎的模板在哪个目录
-app.set('views', __dirname + '/views');
+//#region 2.配置路由
 
-//监听端口，并启动服务
-app.listen(1331,()=>{
-    console.log('server is running at http://localhost:1331');
+
+//#endregion
+
+
+//#region 3.配置ejs模板引擎
+// 3.1 下载ejs包
+// 3.2 设置模板引擎的后缀是什么
+// app.set('view engine', 'ejs');
+// // 3.3 设置模板引擎的模板在哪个目录
+// app.set('views', __dirname + '/views');
+//#endregion
+
+// 监听端口 并启动服务
+app.listen(3008, () => {
+  console.log('server is running at：http://127.0.0.1:3008');
 })
--- XuDan/data/index.js
+++ XuDan/data/index.js
@@ -1,13 +1,13 @@
-// 引入mysql
-const mysql = require("mysql");
+// // 引入mysql
+// const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "192.168.27.200",
-    port: 3306,
-    user: "root",
-    password: "123456",
-    database: "pullradish"
-})
+// const connection = mysql.createConnection({
+//     host: "127.0.0.1",
+//     port: 3306,
+//     user: "root",
+//     password: "123456",
+//     database: "pullradish"
+// })
 
-// 暴露
-module.exports = connection;
+// // 暴露
+// module.exports = connection;
