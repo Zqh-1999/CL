a2c1529761009bf74bf365a3cd33ccdae0cb2cf3
XudanHuazai <2427873127@qq.com>
2019-05-15 09:08:52 +0800

    x

--- Last/controller/CtrlOther.js
+++ Last/controller/CtrlOther.js
@@ -1,7 +1,67 @@
 //引入mysql
 const conn = require('../data')
 
-// shop页面
+// 1.shop页面
 module.exports.shop = (req, res) => {
     res.render('shop');
+}
+
+// 2.beauty页面
+module.exports.beauty = (req, res) => {
+    res.render('beauty');
+}
+
+// 3.bridal页面
+module.exports.bridal = (req, res) => {
+    res.render('bridal');
+}
+
+// 4.man页面
+module.exports.man = (req, res) => {
+    res.render('man');
+}
+
+// 2.beauty页面
+module.exports.beauty = (req, res) => {
+    res.render('beauty');
+}
+
+// 2.beauty页面
+module.exports.beauty = (req, res) => {
+    res.render('beauty');
+}
+
+// 2.beauty页面
+module.exports.beauty = (req, res) => {
+    res.render('beauty');
+}
+
+// 2.beauty页面
+module.exports.beauty = (req, res) => {
+    res.render('beauty');
+}
+
+// 2.beauty页面
+module.exports.beauty = (req, res) => {
+    res.render('beauty');
+}
+
+// 2.beauty页面
+module.exports.beauty = (req, res) => {
+    res.render('beauty');
+}
+
+// 2.beauty页面
+module.exports.beauty = (req, res) => {
+    res.render('beauty');
+}
+
+// 2.beauty页面
+module.exports.beauty = (req, res) => {
+    res.render('beauty');
+}
+
+// 2.beauty页面
+module.exports.beauty = (req, res) => {
+    res.render('beauty');
 }
--- Last/router/routerOther.js
+++ Last/router/routerOther.js
@@ -6,8 +6,35 @@ const router = express.Router();
 //引入controller
 const controller = require('../controller/CtrlOther');
 
-// shop 页面
+// 1.shop 页面
 router.get("/shop", controller.shop);
+
+// 2.beauty 页面
+router.get("/beauty", controller.beauty);
+
+// 3.bridal 页面
+router.get("/bridal", controller.bridal);
+
+// 4.man 页面
+router.get("/man", controller.man);
+
+// 2.beauty 页面
+router.get("/beauty", controller.beauty);
+
+// 2.beauty 页面
+router.get("/beauty", controller.beauty);
+
+// 2.beauty 页面
+router.get("/beauty", controller.beauty);
+
+// 2.beauty 页面
+router.get("/beauty", controller.beauty);
+
+// 2.beauty 页面
+router.get("/beauty", controller.beauty);
+
+// 2.beauty 页面
+router.get("/beauty", controller.beauty);
 
 // 暴露
 module.exports = router;
