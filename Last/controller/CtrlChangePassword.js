857bf43eecefae6f53a0aba035dc6a90c22f21c8
XudanHuazai <2427873127@qq.com>
2019-05-17 14:58:04 +0800

    x

--- Last/controller/CtrlChangePassword.js
+++ Last/controller/CtrlChangePassword.js
@@ -5,3 +5,19 @@ const conn = require('../data')
 module.exports.ChangePassword = (req, res) => {
     res.render('ChangePassword');
 }
+
+
+module.exports.changePassword = (req, res) => {
+   if (req.body.oldPassword == req.session.user.password) {
+       conn.query('update users set password=? where id=?', [req.body.newPassword, req.session.user.id], (err, data) => {
+           if (err) {
+               return console.log(err)
+           }
+           res.json({
+               code: '1101',
+               msg: '密码修改成功！'
+           })
+       })
+
+   }
+} 
