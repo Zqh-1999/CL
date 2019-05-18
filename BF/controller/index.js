89739ac6faf3a88d165a8460a006daaa958ebf6e
ZhangYu <1257068932@qq.com>
2019-05-18 12:35:10 +0800

    1

--- BF/controller/index.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('../data/index')
-
-// 后台首页
-module.exports.BEIndex = (req, res) => {
-  if (!req.session.isLogin) {
-    res.redirect('/admin/login')
-  }
-  res.render('index')
-}
--- BF/route/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const controller = require('../controller/index')
-
-// 后台首页
-router.get('/admin/index', controller.BEIndex)
-
-module.exports = router
